451c39d98bfd4fe8b057acab91856bf6006c03b5
schneekluth <jbetz@posteo.de>
2026-01-30 14:10:04 +0100

    Conversion script + batch video file for ffmpeg and yt-dlp

---
+++ 00_convert.ps1
@@ -0,0 +1,25 @@
+$BASE_PATH = $PSScriptRoot
+$INPUT_VIDEO_FILE = "$BASE_PATH\00_videos.txt"
+
+$fileContent = Get-Content -Encoding UTF8 $INPUT_VIDEO_FILE
+
+foreach ($line in $fileContent) {
+    $name = $line.Split('/')[-1]
+
+    Write-Host "Downloading $line"
+    yt-dlp $line -o "$BASE_PATH\$name"
+}
+
+$files = Get-ChildItem -Path $BASE_PATH\* -File -Exclude @("*.ps1", "*.txt")
+
+foreach ($file in $files) {
+    Write-Host "konvertiere $($file.FullName)"
+    $filenameNoExt = [System.IO.Path]::GetFileNameWithoutExtension($file)
+
+    ffmpeg -y -i $($file.FullName) -pix_fmt yuvj420p -q:v 7 -vf "fps=24,crop=in_w:in_w*4/3:0:(in_h-in_w*4/3)/2,scale=240:-1:flags=lanczos" $BASE_PATH\$filenameNoExt.mjpeg
+
+    Write-Host "lösche $($file.FullName)"
+    Remove-Item -Path $file
+}
+
+Write-Host "`n$($files.Length) Videos gefunden. MAX_FILES auf diesen Wert setzen.`n" -ForegroundColor Yellow
